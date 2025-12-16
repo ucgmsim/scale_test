6fe67bc5e7d3dd66f76ee1151340f561013a9aa4
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2025-12-16 14:07:28 +1300

    misc barcelona fixes

--- flow/lib/python/load_data.py
+++ flow/lib/python/load_data.py
@@ -3,5 +3,8 @@ import csv
 
 def load_data(filepath):
     with open(filepath, 'r') as f:
-        dict_reader = csv.DictReader(filepath)
-        return list(dict_reader)
+        dict_reader = csv.DictReader(f)
+        result = []
+        for row in dict_reader:
+            result.append({k: int(v) for k, v in row.items()})
+    return result
